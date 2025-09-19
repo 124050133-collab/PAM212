e75e30bef1b175e47068a9a94b7354003109e5d7
124050133-collab <124050133@upq.edu.mx>
2025-09-18 22:48:18 -0600

    hice el siguiente codigo sobre el mismo

--- PAM212/Practica1/practica1.js
+++ PAM212/Practica1/practica1.js
@@ -1,8 +1,3 @@
-let nombre = "Armando";
-let edad = 25;
-
-nombre = "Ana Maria";
-
-const saludo = "Hola, " + nombre + ", tienes" + edad + "años.";
-
-console.log(saludo);
+const cuadrado = (numero) => numero * numero;
+console.log(cuadrado(56));  
+console.log(cuadrado(2)); 
