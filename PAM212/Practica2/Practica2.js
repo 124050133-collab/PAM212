ddeedbbf5c0ab6ad0e267954ff4cd494bfb1b564
124050133-collab <124050133@upq.edu.mx>
2025-09-19 18:47:18 -0600

    Extracción de datos

--- PAM212/Practica2/Practica2.js
+++ PAM212/Practica2/Practica2.js
@@ -1,8 +1,11 @@
 const persona = {
     nombre: "Pablo Cipriano",
-    edad: 37,
+    edad: 19,
     direccion: {
         cuidad: "Qro",
         pais: "MX"
     }
 }
+
+const { nombre, edad, direccion: {pais, cuidad }} = persona;
+console.log( "Me llamo " + nombre + " tengo " + edad + " años y vivo en " + cuidad + "."); 
