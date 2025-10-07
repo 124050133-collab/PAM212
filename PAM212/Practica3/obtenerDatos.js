b3085670b76968ff9ff17672a3accef456e47efe
124050133-collab <124050133@upq.edu.mx>
2025-10-07 17:33:50 -0600

    corecciones

--- PAM212/Practica3/obtenerDatos.js
+++ PAM212/Practica3/obtenerDatos.js
@@ -2,18 +2,18 @@ function simularPeticionAPI() {
   return new Promise(resolve => {
     setTimeout(() => {
       resolve("Datos recibidos correctamente");
-    }, 5000); // 5 segundos simulando la espera de la API
+    }, 5000); 
   });
 }
 
 async function obtenerDatos() {
   try {
-    const resultado = await simularPeticionAPI(); // espera la promesa
-    console.log(resultado); // imprime el resultado
+    const resultado = await simularPeticionAPI(); 
+    console.log(resultado); 
   } catch (error) {
     console.error("Error al obtener datos:", error);
   }
 }
 
-// Llamamos a la función
+
 obtenerDatos();
--- PAM212/Practica3/verificarUsuario.js
+++ PAM212/Practica3/verificarUsuario.js
@@ -1,18 +1,20 @@
-function verificarUsuario(usuario){
-    return new Promise ((resolver, reject) => {
-        if(usuario === "admin"){
-             resolver("Acceso concedido");
-        }else{
-            reject("Acceso denegado");
-        } 
-    });
+function verificarUsuario(usuario) {
+  return new Promise((resolve, reject) => {
+    if (usuario === "admin") {
+      resolve("Acceso concedido");
+    } else {
+      reject("Acceso denegado");
+    }
+  });
 }
 
 verificarUsuario("admin")
-.then(res => console.log(res))
-.catch(err => console.log(err));
+  .then(res => console.log(res))  
+  .catch(err => console.log(err));
 
 
-verificarUsuario("Ivan")
-.then(res => console.log(res))
-.catch(err => console.log(err));
+verificarUsuario("Pablo")
+  .then(res => console.log(res))
+  .catch(err => console.log(err)); 
+
+  
