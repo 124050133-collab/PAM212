9d952ce777ed4417f1b019287d70c738c88e5bb2
124050133-collab <124050133@upq.edu.mx>
2025-10-10 18:34:51 -0600

    Practica 6

--- Intro/App.js
+++ Intro/App.js
@@ -1,32 +1,7 @@
-//1. Import zona de declaraciones
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Button } from 'react-native';
-import React, {useState} from 'react';
+import ContadorScreen from './screens/ContadorScreen';
 
-// 2. Zona de componentes
-export default function App() {
-
-  const[contador, setContador]=useState(0);
-  
+export default function App() {  
   return (
-    <View style={styles.container}>
-      <Text> Contador: {contador} </Text>
-      <Button title="Incrementar" onPress={()=>setContador(contador+1)}/>
-      <Button title="Quitar" onPress={()=>setContador(contador-1)}/>
-      <Button title="Reiniciar" onPress={()=>setContador(0)}/>
-
-
-      <StatusBar style="auto" />
-    </View>
+    <ContadorScreen></ContadorScreen>
   );
 }
-
-// 3. Estilos
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Intro/screens/ContadorScreen.js
@@ -0,0 +1,58 @@
+//1. Import zona de declaraciones
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View , Button } from 'react-native';
+import React, {useState} from 'react';
+
+// 2. Zona de componentes
+export default function App() {
+
+  const[contador, setContador]=useState(0);
+  
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}> Contador:  </Text>
+      <Text style={styles.texto2}> {contador} </Text>
+
+      <View style={styles.contenerdorBotones}>
+      <Button color="blue" title="Incrementar" onPress={()=>setContador(contador+1)}/>
+      <Button color="orange" title="Disminuir" onPress={()=>setContador(contador-1)}/>
+      <Button color="red" title="Reiniciar" onPress={()=>setContador(0)}/>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+// 3. Estilos
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7bcbf6ff',
+    alignItems: 'center', //mueve los componentes en el eje horizontalmente o eje x
+    justifyContent: 'center',
+  },
+
+  texto:{
+    color: '#154661ff',
+    fontSize: 30,
+    fontFamily: 'Timer New Roman',
+    fontStyle:'italic',
+    textDecorationLine: 'line-through',
+  },
+
+  texto2:{
+    color: '#005685ff',
+    fontSize: 40,
+    fontFamily: 'Courier',
+    fontWeight: '400',
+    textDecorationLine: 'underline',
+  },
+
+  contenerdorBotones:{
+    marginTop:25,
+    flexDirection: 'row',
+    gap: 15,
+  },
+
+});
